f4bc59adfb5bb0214bad9ba4b239bdfed7d3ee99
Michele <micheleierniero@gmail.com>
2021-11-08 15:12:04 +0100

    modifica

--- laravel-boolpress/resources/views/admin/posts/create.blade.php
+++ laravel-boolpress/resources/views/admin/posts/create.blade.php
@@ -33,7 +33,7 @@
                         <label for="category_id">Categorie: </label>
                         <select name="category_id" id="category">
                             @foreach ($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
                             @endforeach
                         </select>
                     </div>
